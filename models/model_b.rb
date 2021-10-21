72dad46cab1f7fe18bc6356c9c34557ae47f0dbb
Sam Smith <samlsmith424@gmail.com>
2021-10-21 10:58:22 -0700

    Add eight keys to vehicle hash

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+vehicle = {
+  brand: "audi",
+  model: "tt",
+  year: 2020,
+  zero_to_sixty: 3.5,
+  is_operating: true,
+  for_sale: false,
+  interior: ['leather', 'cream', 'black'],
+  exterior: ['matte black', 'chrome', 'spinners']
+
+}
