838d6ad6c2d341ff5439df338eaa542027cd94bf
Sam Smith <samlsmith424@gmail.com>
2021-10-21 10:52:32 -0700

    Add eight total keys to hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+airport_hash = {
+  name: "sfo",
+  location: "california",
+  number_of_employees: 2000,
+  terminals: 5.0,
+  is_open: true,
+  sometimes_closes: false,
+  kiosks: ['books', 'food', 'clothing'],
+  type_of_travelers: ['business', 'vacation', 'frequent fliers']
+}
