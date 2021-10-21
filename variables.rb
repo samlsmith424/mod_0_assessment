52f2da67867ae4257129dd681f16c472e29263ca
Sam Smith <samlsmith424@gmail.com>
2021-10-21 10:41:33 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+famous_person = "britney_spears"
+
+is_active = true
+
+famous_person = "madonna"
+
+num_3 = 6
+num_9 = 12
+
+sum = "#{num_3 + num_9}"
+
+animals = ['lions', 'tigers', 'bears']
+
+animals.shift
+puts animals
+
+puts famous_person.length
